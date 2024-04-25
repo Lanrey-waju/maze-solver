e6f1eb353e26b5f2f070174e272501bf0c357413
Abdulmumin Akinde <lanreywaju97@gmail.com>
2024-04-25 21:56:55 +0100

    Create cell class

--- graphic.py
+++ graphic.py
@@ -10,6 +10,9 @@ class Window:
         self.running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.canvas, fill_color)
+
     def redraw(self):
         self.__root.update_idletasks()
         self.__root.update()
@@ -21,3 +24,47 @@ class Window:
 
     def close(self):
         self.running = False
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Line:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(
+            self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2
+        )
+
+
+class Cell:
+    def __init__(self, window):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._win = window
+        self._x1 = None
+        self._y1 = None
+        self._x2 = None
+        self._y2 = None
+
+    def draw(self, x1, y1, x2, y2):
+        self._x1 = x1
+        self._y1 = y1
+        self._x2 = x2
+        self._y2 = y2
+        if self.has_left_wall:
+            self._win.draw_line(Line(Point(x1, y1), Point(x1, y2)))
+        if self.has_right_wall:
+            self._win.draw_line(Line(Point(x2, y1), Point(x2, y2)))
+        if self.has_top_wall:
+            self._win.draw_line(Line(Point(x1, y1), Point(x2, y1)))
+        if self.has_bottom_wall:
+            self._win.draw_line(Line(Point(x1, y2), Point(x2, y2)))
